5aa830d6448546a39865f1b5b4f441afd1890cac
Gnaneshwar Pochammala <pochammalagnaneshwar912@gmail.com>
2026-09-08 21:30:43 +0530

    Time: 3 ms (86.08%), Space: 43.3 MB (40.88%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }
+            if(ch==')'){
+            if(!st.isEmpty()&&st.peek()=='('){
+                st.pop();
+
+            }
+            else{
+                return false;
+            }
+            }
+            if(ch==']'){
+            if(!st.isEmpty()&&st.peek()=='['){
+                st.pop();
+
+            }
+            else{
+                return false;
+            }
+            }
+            if(ch=='}'){
+            if(!st.isEmpty()&&st.peek()=='{'){
+                st.pop();
+
+            }
+            else{
+                return false;
+            }
+        }
+        }
+        return st.isEmpty();
+        
+    }
+}
