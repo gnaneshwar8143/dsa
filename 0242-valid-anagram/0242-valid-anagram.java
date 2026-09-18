ef8abebda0cf56bedbc177bc62ed84678552ed43
Gnaneshwar Pochammala <pochammalagnaneshwar912@gmail.com>
2026-09-19 00:57:44 +0530

    Time: 17 ms (16.63%), Space: 46.4 MB (33.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer>map=new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            int count=map.getOrDefault(ch,0);
+            map.put(ch,count+1);
+            
+
+        }
+        for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+            if(!map.containsKey(c)){
+                return false;
+            }
+            map.put(c,map.get(c)-1);
+            if(map.get(c)<0){
+                return false;
+            }
+        }
+        return true;
+        
+        
+    }
+}
