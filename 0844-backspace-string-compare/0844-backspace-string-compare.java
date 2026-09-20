085d2ee14a0549a82570e7c6d67c01cbd36c8390
Gnaneshwar Pochammala <pochammalagnaneshwar912@gmail.com>
2026-09-21 00:46:39 +0530

    Time: 2 ms (59.48%), Space: 42.9 MB (78.88%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character>st=new Stack<>();
+        Stack<Character>st1=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch =='#'){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(ch);
+            }
+           
+          
+        }
+         for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+            if(c=='#'){
+                if(!st1.isEmpty()){
+                    st1.pop();
+                }
+            }
+            else{
+                st1.push(c);
+            }
+           
+                
+            
+           
+        }
+        if(!st.equals(st1)){
+            return false;
+        }
+        return true;
+    }
+}
